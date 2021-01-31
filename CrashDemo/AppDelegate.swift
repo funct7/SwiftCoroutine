db5aebe43eb07fc1f28595d29f45315c4ab68780
Josh Woomin Park <joshuaparkwm@gmail.com>
2021-01-31 14:46:40 +0900

    Add: CrashDemo target

---
+++ CrashDemo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CrashDemo
+//
+//  Created by Josh Woomin Park on 2021/01/31.
+//  Copyright © 2021 Alex Belozierov. All rights reserved.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
---
+++ CrashDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrashDemo
+//
+//  Created by Josh Woomin Park on 2021/01/31.
+//  Copyright © 2021 Alex Belozierov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
